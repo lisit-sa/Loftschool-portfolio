fab0ce0bae230cf9b82d7212e11301a146110843
lisit-sa <kristaliska@gmail.com>
2015-12-04 01:01:05 +0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+	browserSync = require("browser-sync");
+gulp.task('server',function (){
+	browserSync({
+		port: 9000,
+		server: {
+			baseDir: 'app'
+		}
+	});
+});
+gulp.task('watch', function() {
+	gulp.watch([
+		'app/index.html',
+		'app/css/**/*.css',
+		'app/js/**/*.js'
+	]).on('change', browserSync.reload);
+});
+gulp.task('default', ['server', 'watch']);
