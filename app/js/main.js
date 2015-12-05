ba644484503798eef02b23dc23d2f757038fc293
lisit-sa <kristaliska@gmail.com>
2015-12-05 12:07:19 +0400

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
-jQuery('input[placeholder], textarea[placeholder]').placeholder();
+/* Функция закрывает по нажатие на кнопку модальное окно */
+function bclose() {
+	$(".pop-up").bPopup().close();
+	return false;
+}
+
+/* Функция показывает модальное окно по нажатию на кнопку */
+function bopen() {
+	$('.pop-up').bPopup();
+	return false;
+}
