37f3902de79f7bd8146d3becf0f65d5043dc8cb4
lisit-sa <kristaliska@gmail.com>
2015-12-07 21:07:31 +0400

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,26 @@ function bopen() {
 	$('.pop-up').bPopup();
 	return false;
 }
+
+var close = document.querySelector(".pop-up-close");
+var popup = document.querySelector(".pop-up");
+
+      link.addEventListener("click", function(event) 
+      {
+        event.preventDefault();
+        popup.classList.add("write-us-show");
+      login.focus();
+      if (storage) {
+        login.value = storage;
+        e-mail.focus();
+      } else {
+        login.focus();
+      }
+
+
+      });
+      close.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("write-us-show");
+        popup.classList.remove("write-error");
+      });
